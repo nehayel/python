d572413d3b33eaf20645572750c9587dc889ac0e
nehayel <walidhossain179@gmail.com>
2026-06-11 23:07:40 +0600

    variable and strng

---
+++ string.py
@@ -0,0 +1,18 @@
+first_name = "walid"
+last_name = "Hossain"
+gpa = 4.00
+
+full_name = f"{first_name} {last_name}. \n\tYou got gpa {gpa}"
+print(full_name.title())
+
+name = "            nehayel         "
+print(name.strip().title())
+
+a = "       faltu  "
+print(a.lstrip())
+
+b = "  faltu  "
+print(b.rstrip())
+
+intro = 'hello "walid hossain" walid'
+print(intro)
