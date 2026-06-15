e915ac430d2521cd88f5686c26c4b4a0624cc6d9
nehayel <walidhossain179@gmail.com>
2026-06-16 00:30:43 +0600

    list

---
+++ list.py
@@ -0,0 +1,13 @@
+nums = [1,2,3,4,5,6,7,8,9]
+
+print(nums)
+
+print(nums[6])
+
+print(nums[-1])
+
+names = ["wlaid", "Hossain", "Faltu", "Ahammok"]
+
+print(names)
+
+print(names[1].upper())
---
+++ numbers.py
@@ -0,0 +1,7 @@
+num, num1, num2= 3, 4, 5
+print(num)
+
+print(num1, num2)
+
+num = 1_000_000_000
+print(num)
