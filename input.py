48789ff95aaccfa0d6b30b57a3f2c05c95a74b3f
nehayel <walidhossain179@gmail.com>
2026-08-25 20:01:08 +0600

    input

---
+++ input.py
@@ -0,0 +1,17 @@
+# age = input("What is your age?")
+#
+# age = int(age)
+#
+# if age >= 18:
+#     print("Access Granted")
+#
+# else:
+#     print("Access Denied")
+#
+
+num = int(input("Enter a number: "))
+
+if num % 2 == 0:
+    print(f"{num} is even")
+else:
+    print(f"{num} is odd")
