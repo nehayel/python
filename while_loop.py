c7302ba1ac556aaf34fc7d7d22073b5e0b7a36e3
nehayel <walidhossain179@gmail.com>
2026-08-26 00:45:10 +0600

    while loop

---
+++ while_loop.py
@@ -0,0 +1,38 @@
+# num = 10
+# while num < 50:
+#     print(num)
+#     num += 1
+
+
+# count = 0
+#
+#
+# while count < 10:
+#     name = input()
+#     print(f"welcome to out class {name.title()}")
+#     count += 1
+
+
+# activate = True
+# while activate:
+#     name = input()
+#     if name == "quit":
+#         activate = False
+#     else:
+#         print(f"welcome to out class {name.title()}")
+
+
+# nums = 100
+# count = 0
+# while count <= 100:
+#     if count % 2 == 0:
+#         print(count)
+#     count += 1
+
+count = 0
+while count <= 100:
+    count += 1
+
+    if count % 2 == 0:
+        continue
+    print(count)
