76c7060b719c9ea7cc00f6e489c7302f8e4e9361
nehayel <walidhossain179@gmail.com>
2026-08-30 23:49:36 +0600

    function argument

---
+++ function_argument.py
@@ -0,0 +1,12 @@
+def even_odd(num):
+    if num % 2 ==0:
+        print(f"{num} is an even number")
+    else:
+        print(f"{num} is an odd number")
+
+even_odd(2)
+
+def power(num, pow):
+    print(num ** pow)
+
+power(2, 2)
